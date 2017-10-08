9889ba98d92e7be06d0d8dd98776a213cd0cc559
Mateusz Warzyński <mathew.warzynski@gmail.com>
2017-10-08 02:42:15 +0200

    Added more commands for VIM configuration

--- install.sh
+++ install.sh
@@ -11,14 +11,22 @@ chsh -s /bin/zsh
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 
 # Get dotfiles.
-git clone git@github.com:mwarzynski/dotfiles.git "${HOME}/"
+git clone git@github.com:mwarzynski/dotfiles.git "${HOME}/dotfiles"
 
-# Create TODO file.
+# Install nvim
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt -y install neovim
+
+# Install Deoplete
+sudo apt -y install python3-pip
+pip3 install neovim
+
+# Install plugin manager for nvim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+# Create file for todo notes.
 touch ~/.todo
-
-# Show README
-cat README.md
-
-# Prompt for relogin.
-echo "Please, relogin."
 
