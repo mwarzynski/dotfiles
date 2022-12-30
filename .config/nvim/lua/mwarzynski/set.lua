e747e8ac22e1cd7886132029c283010810870bfc
Mateusz Warzyński <mateusz@warzynski.pro>
2022-12-30 01:44:41 +0100

    fix(nvim): replace tabs with 4 spaces by default

--- .config/nvim/lua/mwarzynski/set.lua
+++ .config/nvim/lua/mwarzynski/set.lua
@@ -3,10 +3,9 @@ vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.shiftwidth = 4
-vim.expandtab = true
-
-vim.smartindent = true
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
