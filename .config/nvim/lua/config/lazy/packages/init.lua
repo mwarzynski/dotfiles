3592affb604fb37ceb67a8b0c5a5ea028eec829b
Mateusz Warzynski <mateusz@warzynski.pro>
2025-04-29 02:31:19 +0200

    nvim: add todo-comments plugin

--- .config/nvim/lua/config/lazy/packages/init.lua
+++ .config/nvim/lua/config/lazy/packages/init.lua
@@ -4,6 +4,7 @@ return {
     "RRethy/nvim-base16",
     "mbbill/undotree",
     { "nvim-telescope/telescope.nvim", tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' } },
+    "folke/todo-comments.nvim",
 
     require("config.lazy.packages.p_git"),
     require("config.lazy.packages.p_lsp"),
